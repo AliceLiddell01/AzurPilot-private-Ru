b92b7b43459d9ee04445707399f4ad67e7f9aa5a
AliceLiddell01 <leaf.fairy@proton.me>
2026-08-08 04:31:50 +0700

    feat(research): русифицировать сообщение preset generator

--- module/research/preset_generator.py
+++ module/research/preset_generator.py
@@ -38,7 +38,7 @@ def translate(string: str, target='series_4_tenrai_only_cube', for_simulate=Fals
     if res:
         series = res.group(1)
     else:
-        print(f'Translate target from unknown series: {target}')
+        print(f'Неизвестная серия для преобразования цели: {target}')
         return
     cube = 'cube' in target
     string = string.replace('S4-H0.5 > !4-0.5', '0.5')
