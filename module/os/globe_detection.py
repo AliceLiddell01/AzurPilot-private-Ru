5f4fe34bbab76555d9ba398a14c2bb421847a65a
AliceLiddell01 <leaf.fairy@proton.me>
2026-08-09 16:26:09 +0700

    fix(i18n): восстановить docstring без изменений

--- module/os/globe_detection.py
+++ module/os/globe_detection.py
@@ -120,7 +120,7 @@ class GlobeDetection:
             points (np.ndarray): 全球地图坐标点数组。
 
         Returns:
-            np.ndarray: 对应的全球地图坐标点数组。
+            np.ndarray: 对应的屏幕坐标点数组。
         """
         return perspective_transform(points, data=self.homography.homo_invt)
 
