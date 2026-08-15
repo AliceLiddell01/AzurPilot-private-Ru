8bc85b53633412f15a865cff34f0c800088af458
AliceLiddell01 <leaf.fairy@proton.me>
2026-08-15 15:59:20 +0700

    feat(recovery): применять Stage 3 migration при загрузке профиля

--- module/config/config.py
+++ module/config/config.py
@@ -21,6 +21,7 @@ from module.config.config_manual import ManualConfig, OutputConfig
 from module.config.config_updater import ConfigUpdater, ensure_time, get_server_next_update, nearest_future
 from module.config.deep import deep_get, deep_set
 from module.config.opsi_data_logger import data_logger_is_active_from_data
+from module.config.recovery_default_on_migration import apply_recovery_default_on_migration
 from module.config.time_source import now as current_time
 from module.config.utils import DEFAULT_TIME, dict_to_kv, filepath_config, get_os_reset_remain, path_to_arg, is_good_gpu
 from module.config.watcher import ConfigWatcher
@@ -189,6 +190,16 @@ class AzurLaneConfig(ConfigUpdater, ManualConfig, GeneratedConfig, ConfigWatcher
 
     def load(self):
         self.data = self.read_file(self.config_name)
+        if (
+            not self.is_template_config
+            and os.path.exists(filepath_config(self.config_name))
+            and apply_recovery_default_on_migration(self.data)
+        ):
+            logger.info(
+                "[Конфигурация] Stage 3: unattended emulator recovery включён "
+                "для существующего профиля; migration marker сохранён"
+            )
+            self.write_file(self.config_name, data=self.data)
         self.config_override()
 
         for path, value in self.modified.items():
@@ -670,7 +681,7 @@ class AzurLaneConfig(ConfigUpdater, ManualConfig, GeneratedConfig, ConfigWatcher
         - 任务可能被用户禁用
 
         Args:
-            task (str): 要调用的任务名称，如 `Restart`。
+            task (str): 要调用另一个任务名称，如 `Restart`。
             force_call (bool): 是否强制调用。
 
         Returns:
